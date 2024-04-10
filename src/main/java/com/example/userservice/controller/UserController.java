b99beb040cc3b13ce5125a04f2ce23e966926d2a
MinhHong <minhhong1612@gmail.com>
2024-04-10 15:11:29 +0700

    user service

---
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.models.User;
+import com.example.userservice.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+    @GetMapping("/users")
+    public List<User> getAll(){
+        List<User> list = userRepository.findAll();
+        return list;
+    }
+
+    @GetMapping("/users/{id}")
+    public Optional<User> getUserById(@PathVariable("id") long id){
+        Optional<User> user =   userRepository.findById(id);
+        return user;
+    }
+
+    @PostMapping("/user")
+    public User create(@RequestBody User user){
+        user = userRepository.save(user);
+        return user;
+    }
+
+}
